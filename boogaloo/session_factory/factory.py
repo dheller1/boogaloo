2bf434d6c30cab64acada7e9d3b202cc51f152d2
Dominik Heller <dominik.heller1@gmail.com>
2020-02-16 13:39:13 +0100

    implement session_factory.Factory to create the session tree from the definition tree

--- boogaloo/session_factory/factory.py
+++ boogaloo/session_factory/factory.py
@@ -1,12 +1,30 @@
 from boogaloo.definition.entities.cards import HandDefinition
+from boogaloo.session.actors.gameactor import GameActor
 from boogaloo.session_factory.builders.handofcardsbuilder import HandOfCardsBuilder
+
+import logging
 
 
 class Factory:
-    builders = {
+    Builders = {
         HandDefinition: HandOfCardsBuilder(),
     }
 
     @staticmethod
     def build_tree(tree_def):
-        pass
+        game = GameActor()
+        for node in tree_def.root.globals:
+            Factory.build_node(game, node)
+
+        for node in tree_def.root.per_player:
+            pass
+
+    @staticmethod
+    def build_node(owner, node_def):
+        if node_def.entity is not None:
+            builder = Factory.Builders.get(type(node_def.entity))
+            if not builder:
+                logging.error(f'No builder is defined for type {type(node_def.entity)}.')
+                return None
+
+            return builder.create(owner, node_def.entity)
